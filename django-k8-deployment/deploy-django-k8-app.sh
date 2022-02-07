011337f130956fc67839fa7f7e6357b041a12e9d
Masud Petronia <scriptcode@outlook.com>
2022-02-07 22:10:15 +0100

    Wrong paths

--- django-k8-deployment/deploy-django-k8-app.sh
+++ django-k8-deployment/deploy-django-k8-app.sh
@@ -22,12 +22,12 @@ find .
 # Create a namespace
 echo "=NAMESPACE ====="
 echo "Namespace: ${K8_NAMESPACE}"
-envsubst < configs/namespaces/capstone.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/namespaces/capstone.yaml | kubectl apply -f -
 
 echo -e "\n=CONFIGMAPS ====="
 # Create the configmaps for django and postgres
-envsubst < configs/configmaps/django-config.yaml | kubectl apply -f -
-envsubst < configs/configmaps/postgres-config.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/configmaps/django-config.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/configmaps/postgres-config.yaml | kubectl apply -f -
 
 # Create the configmap for traefik (2-step)
 # STEP 1: Include domain name in the yaml snippet
@@ -35,17 +35,17 @@ echo "Variables used:"
 echo "domain: $DOMAIN"
 echo "www domain: www.$DOMAIN"
 echo "email: $DOMAIN_EMAIL"
-sed -e "s/\${domain}/$DOMAIN/" -e "s/\${domain_email}/$DOMAIN_EMAIL/"  -e "s/\${wwwdomain}/$WWWDOMAIN/" configs/.envs/.configmaps/.traefik >> configs/traefik/traefik.yml
+sed -e "s/\${domain}/$DOMAIN/" -e "s/\${domain_email}/$DOMAIN_EMAIL/"  -e "s/\${wwwdomain}/$WWWDOMAIN/" ./django-k8-deployment/configs/.envs/.configmaps/.traefik >> ./django-k8-deployment/configs/traefik/traefik.yml
 # STEP 2: Create the configmap
-kubectl create configmap envs-production-traefik -n "${K8_NAMESPACE}" --from-env-file=configs/traefik/traefik.yml
-envsubst < configs/configmaps/traefik-configmap.yaml | kubectl apply -f -
+kubectl create configmap envs-production-traefik -n "${K8_NAMESPACE}" --from-env-file=./django-k8-deployment/configs/traefik/traefik.yml
+envsubst < ./django-k8-deployment/configs/configmaps/traefik-configmap.yaml | kubectl apply -f -
 
 echo -e "\n=SECTRETS ====="
 # Create secrets
 export DSK=$(echo -n "$DJANGO_SECRET_KEY" | base64)
-envsubst < configs/secrets/django.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/secrets/django.yaml | kubectl apply -f -
 export PGPW=$(echo -n "$POSTGRES_PASSWORD" | base64)
-envsubst < configs/secrets/postgres.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/secrets/postgres.yaml | kubectl apply -f -
 
 echo -e "\n=STORAGECLASS ====="
 # Create storageclass
@@ -54,23 +54,23 @@ echo -e "\n=STORAGECLASS ====="
 # https://stackoverflow.com/questions/51946393/kubernetes-pod-warning-1-nodes-had-volume-node-affinity-conflict
 export AZONE=${DJANGO_AWS_REGION}${AVAILABILITY_ZONE_SUFFIX}
 echo "Availability Zone used for AWS EBS: $AZONE"
-envsubst < configs/storageclass/storageclass-ingress.yaml | kubectl apply -f -
-envsubst < configs/storageclass/storageclass-postgres.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/storageclass/storageclass-ingress.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/storageclass/storageclass-postgres.yaml | kubectl apply -f -
 
 echo -e "\n=VOLUMECLAIMS ====="
 # Create volumes claims
-envsubst < configs/volume-claims/postgres-backups-pv-claim.yaml | kubectl apply -f -
-envsubst < configs/volume-claims/postgres-data-pv-claim.yaml | kubectl apply -f -
-envsubst < configs/volume-claims/traefik-pv-claim.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/volume-claims/postgres-backups-pv-claim.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/volume-claims/postgres-data-pv-claim.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/volume-claims/traefik-pv-claim.yaml | kubectl apply -f -
 
 echo -e "\n=DEPLOYMENTS ====="
-envsubst < configs/deployments/django-deployment.yaml | kubectl apply -f -
-envsubst < configs/deployments/postgres-deployment.yaml | kubectl apply -f -
-envsubst < configs/deployments/redis-deployment.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/deployments/django-deployment.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/deployments/postgres-deployment.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/deployments/redis-deployment.yaml | kubectl apply -f -
 
 echo -e "\n=INGRESS PT1 ====="
 # Create ingress service
-envsubst < configs/ingress-p1/traefik-service.yaml | kubectl apply -f -
+envsubst < ./django-k8-deployment/configs/ingress-p1/traefik-service.yaml | kubectl apply -f -
 
 echo -e "\n=Pausing 10 seconds for the external IP to populate"
 sleep 10
@@ -91,7 +91,7 @@ read -p "Are you sure?[yN] " -n 1 -r
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    envsubst < configs/ingress-p2/traefik-controller-deployment.yaml | kubectl apply -f -
+    envsubst < ./django-k8-deployment/configs/ingress-p2/traefik-controller-deployment.yaml | kubectl apply -f -
 else
     exit 0
 fi
